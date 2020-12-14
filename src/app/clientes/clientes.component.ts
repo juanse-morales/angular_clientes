264a89717c8c357ef6810f4e9fab0b83b63de581
Juan Morales <jmorales@dcatech.com>
2020-12-14 17:55:15 -0500

    Clase 57: Validando los clientes en la tabla HTML con ngIf

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 
@@ -18,6 +19,48 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    });
+    
+    swalWithBootstrapButtons.fire({
+      title: 'Estás seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre+' '+cliente.apellido}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, Eliminar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.clienteService.delete(cliente.id).subscribe(response=>{
+          this.clientes = this.clientes.filter(cli => cli !== cliente);
+          swalWithBootstrapButtons.fire(
+            'Cliente Eliminado!',
+            `El cliente ${cliente.nombre+' '+cliente.apellido} ha sido eliminado con éxito.`,
+            'success'
+          );
+        });
+        
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'Tu cliente está seguro :)',
+          'error'
+        )
+      }
+    });
   }
 
 }
---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  public cliente: any = new Cliente();
+  public titulo: string = "Crear cliente";
+
+  constructor(private clienteService: ClienteService, 
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.cargarCliente();
+  }
+
+  public cargarCliente(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      if(id){
+        this.clienteService.getCliente(id).subscribe(cliente=>this.cliente=cliente);
+      }
+    });
+  }
+
+  public create(): void {
+    this.clienteService.create(this.cliente).subscribe(
+      data => {
+        this.router.navigate(['/clientes']);
+        swal.fire('Nuevo cliente', `Cliente ${data.cliente.nombre +' '+data.cliente.apellido} creado con éxito!`, 'success');
+      }
+    );
+  }
+
+  update(): void {
+    this.clienteService.update(this.cliente).subscribe( data => {
+      this.router.navigate(['/clientes']);
+      swal.fire('Cliente Actualizado', `Cliente ${data.cliente.nombre+' '+data.cliente.apellido} actualizado con éxito!`, 'success');
+    })
+  }
+
+}
